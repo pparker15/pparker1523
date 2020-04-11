e08ca9a3e829495579733bbe8f660adf0054a7f2
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-11 18:51:44 +0100

    Create configUbuntu.sh
    This installs nfdump, mongo client and pymongo.

---
+++ configUbuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+# install nfdump
+
+cd /usr/src/
+apt-get install libtool -y
+apt-get install bison
+apt-get install libbz2.dev -y
+apt install make gcc flex rrdtool librrd-dev libpcap-dev php librrds-perl libsocket6-perl apache2 -y
+wget https://github.com/phaag/nfdump/archive/v1.6.17.tar.gz
+tar xzfv v1.6.17.tar.gz
+cd nfdump-1.6.17/
+sh ./autogen.sh
+./configure --enable-nsel --enable-nfprofile --enable-sflow --enable-readpcap --enable-nfpcapd
+make
+make install
+cd /usr/local/lib
+ldconfig -v
+
+#install mongo database
+
+apt install mongodb-clients -y
+apt install python3-pip -y
+pip3 install pymongo
+
+#install whois
+apt install whois
