9f34bfbaaf518686d2b20df309f581fd29e4324c
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-12 16:22:45 +0100

    Update configureMongo.sh
    IP address was supposed to be the one of the host not the remote machine

--- configureMongo.sh
+++ configureMongo.sh
@@ -5,5 +5,5 @@ systemctl enable mongodb
 
 mongo < mongotest.js
 sed -i 's/#auth = true/auth = true/g' /etc/mongodb.conf 
-read -p "Enter IP address of remote system: " ipv4
+ipv4=$(hostname -I)
 sed -i "s/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,$ipv4/g" /etc/mongodb.conf
