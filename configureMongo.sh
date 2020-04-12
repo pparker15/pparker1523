3c401d4211eaa006718c81f55c691c48f7f8ec8d
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-12 16:23:44 +0100

    Update configureMongo.sh
    Different name for the user file

--- configureMongo.sh
+++ configureMongo.sh
@@ -3,7 +3,7 @@
 apt install -y mongodb
 systemctl enable mongodb
 
-mongo < mongotest.js
+mongo < createUserMongo.js
 sed -i 's/#auth = true/auth = true/g' /etc/mongodb.conf 
 ipv4=$(hostname -I)
 sed -i "s/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,$ipv4/g" /etc/mongodb.conf
