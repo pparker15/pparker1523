cacb6dd48174b33e69e0d2749e31f0ff15f584ad
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-18 16:18:19 +0100

    Update IdentificationByAssociation.py
    Spotify has been updated

--- IdentificationByAssociation.py
+++ IdentificationByAssociation.py
@@ -1,8 +1,4 @@
 from datetime import datetime, timedelta, time
-facebook = "n"
-twitter = "n"
-bbc = "n"
-spotify = "n"
 spotifyTime = []
 facebookTime = []
 previousTime = " "
@@ -11,54 +7,74 @@ bbcTime = []
 
 # SPOTIFY
 
-# Check to see if there is a streaming service running in the file.
-# Do I need to only get the last request time not response time?
 with open("spotifyfacebooktwitterbbcsky.txt") as file:
     number = -1
     for line in file:
         if "SPOTIFY" in line:
-            spotify = "y"
             splitLine = line.split()
-            spotifyTime.append(splitLine[2] + " " +splitLine[4])
+            spotifyTime.append(datetime.strptime(splitLine[2] + " " + splitLine[4], '%Y-%m-%d %H:%M:%S.%f'))
             number += 1
 
+previous = " "
+StartTimes = []
+newTimesList = []
+newTimesList.append([])
+loopNum = 0
+newStart = 0
+secondNum = 0
+for time in spotifyTime:
+    if previous != " ":
+        # Time may need tweeking
+        newTime = previous + timedelta(minutes = 10)
+        # Less than
+        if(time <= newTime):
+            newTimesList[secondNum].append(time)
+        else:
+            newTimesList.append([])
+            secondNum += 1
+            newTimesList[secondNum].append(time)
+        previous = time
+    else:
+        previous = time
+        newTimesList[secondNum].append(time)
 
-# Convert to  time format to add and take away time
-firstSpotify = datetime.strptime(spotifyTime[0], '%Y-%m-%d %H:%M:%S.%f')
-estFirstSpotify = firstSpotify - timedelta(minutes = 10)
-lastSpotify = datetime.strptime(spotifyTime[number], '%Y-%m-%d %H:%M:%S.%f')
-estLastSpotify = lastSpotify + timedelta(minutes = 3)
-#print(estFirstSpotify, estLastSpotify)
+num = -1
+for element in newTimesList:
+    length = len(element) - 1
+    num += 1
+    if(length >= 2):
+        startTime = newTimesList[num][0] - timedelta(minutes = 10)
+        endTime = newTimesList[num][length] + timedelta(minutes = 10)
+        StartTimes.append((str(startTime) + "/" + str(endTime)))
 
-
+numTime = 0
 with open("spotifyfacebooktwitterbbcsky.txt") as file:
     for line in file:
         if "SOCIAL MEDIA" not in line:
             if "NEWS" not in line:
                 # if the time on the line is after estFirstSpotify but before spotify
+                times = StartTimes[numTime]
+                splitTime = times.split("/")
+                lineSplit = line.split()
                 splitLine = line.split()
                 lineTime = splitLine[2] + " " + splitLine[4]
-                lineTime = datetime.strptime(lineTime, '%Y-%m-%d %H:%M:%S.%f')
-                #print out associated spotify if within the time frame otherwise just print the line
-                if lineTime >= estFirstSpotify and lineTime <= estLastSpotify:
+                if lineTime + "000" >= splitTime[0] and lineTime + "000" <= splitTime[1]:
                     fileOut = open("AfterSpotify.txt", "a")
                     fileOut.write(str(line.rstrip() + " - " + "***ASSOCIATED SPOTIFY***"))
                     fileOut.write('\n')
                     fileOut.close()
-                else:
-                    fileOut = open("AfterSpotify.txt", "a")
-                    fileOut.write(str(line))
-                    fileOut.close()
-            # Not spotify so print out the line
             else:
                 fileOut = open("AfterSpotify.txt", "a")
                 fileOut.write(str(line))
                 fileOut.close()
-        #not spotify so print out the line
         else:
             fileOut = open("AfterSpotify.txt", "a")
             fileOut.write(str(line))
             fileOut.close()
+        if lineTime + "000" == splitTime[1]:
+            comNum = len(StartTimes) - 1
+            if numTime != comNum:
+                numTime += 1
 
 # FACEBOOK
 
