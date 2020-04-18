e2bdb2b3a8551298a600994c612fac9b16f6a9df
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-18 11:59:08 +0100

    Update IdentificationByAssociation.py
    Updated to include twitter and bbc.

--- IdentificationByAssociation.py
+++ IdentificationByAssociation.py
@@ -6,6 +6,8 @@ spotify = "n"
 spotifyTime = []
 facebookTime = []
 previousTime = " "
+twitterTime = []
+bbcTime = []
 
 # SPOTIFY
 
@@ -58,6 +60,7 @@ with open("spotifyfacebooktwitterbbcsky.txt") as file:
             fileOut.write(str(line))
             fileOut.close()
 
+# FACEBOOK
 
 # look for facebook in the line and add the time to a list.
 with open("AfterSpotify.txt") as file:
@@ -75,6 +78,7 @@ newStart = 0
 secondNum = 0
 for time in facebookTime:
     if previous != " ":
+        # Time may need tweeking
         newTime = previous + timedelta(seconds = 15)
         #print(time, newTime)
         # Less than
@@ -97,13 +101,11 @@ num = -1
 for element in newTimesList:
     length = len(element) - 1
     num += 1
-    if(length >= 6):
+    if(length >= 4):
         startTime = newTimesList[num][0]
         endTime = newTimesList[num][length]
         StartTimes.append((str(startTime) + "/" + str(endTime)))
-
-
-
+        
 numTime = 0
 with open("AfterSpotify.txt") as file:
     for line in file:
@@ -132,4 +134,141 @@ with open("AfterSpotify.txt") as file:
             print(numTime, comNum)
             if numTime != comNum:
                 numTime += 1
-                print("NUMTIME USED")
+
+# TWITTER
+# look for twitter in the line and add the time to a list.
+with open("AfterFacebook.txt") as file:
+    for line in file:
+        if "TWITTER" in line:
+            splitLine = line.split()
+            twitterTime.append(datetime.strptime(splitLine[3] + " " + splitLine[5], '%Y-%m-%d %H:%M:%S.%f'))
+
+previous = " "
+StartTimes = []
+newTimesList = []
+newTimesList.append([])
+loopNum = 0
+newStart = 0
+secondNum = 0
+for time in twitterTime:
+    if previous != " ":
+        # Time may need tweeking
+        newTime = previous + timedelta(seconds = 30)
+        # Less than
+        if(time <= newTime):
+            newTimesList[secondNum].append(time)
+        else:
+            newTimesList.append([])
+            secondNum += 1
+            newTimesList[secondNum].append(time)
+        previous = time
+    else:
+        previous = time
+        newTimesList[secondNum].append(time)
+
+num = -1
+for element in newTimesList:
+    length = len(element) - 1
+    num += 1
+    if(length >= 6):
+        startTime = newTimesList[num][0]
+        endTime = newTimesList[num][length]
+        StartTimes.append((str(startTime) + "/" + str(endTime)))
+        
+numTime = 0
+with open("AfterFacebook.txt") as file:
+    for line in file:
+        times = StartTimes[numTime]
+        splitTime = times.split("/")
+        lineSplit = line.split()
+        if "SOCIAL MEDIA" in line:
+            lineTime = lineSplit[3] + " " + lineSplit[5]
+        else:
+            lineTime = lineSplit[2] + " " + lineSplit[4]
+        if lineTime + "000" >= splitTime[0] and lineTime + "000" <= splitTime[1]:
+            if "STREAMING" or "NEWS" or "FACEBOOK" not in line:
+                fileOut = open("AfterTwitter.txt", "a")
+                fileOut.write(str(line.rstrip() + " - " + "***ASSOCIATED TWITTER***"))
+                fileOut.write('\n')
+                fileOut.close()
+        else:
+            # Prints out the beginning of the file but not the end. Why?
+            fileOut = open("AfterTwitter.txt", "a")
+            fileOut.write(line.rstrip())
+            fileOut.write('\n')
+            fileOut.close()
+        if lineTime + "000" == splitTime[1]:
+            print(numTime, len(StartTimes))
+            comNum = len(StartTimes) - 1
+            print(numTime, comNum)
+            if numTime != comNum:
+                numTime += 1
+
+# BBC
+# look for BBC in the line and add the time to a list.
+with open("AfterTwitter.txt") as file:
+    for line in file:
+        if "BBC" in line:
+            splitLine = line.split()
+            bbcTime.append(datetime.strptime(splitLine[2] + " " + splitLine[4], '%Y-%m-%d %H:%M:%S.%f'))
+
+previous = " "
+StartTimes = []
+newTimesList = []
+newTimesList.append([])
+loopNum = 0
+newStart = 0
+secondNum = 0
+for time in bbcTime:
+    if previous != " ":
+        # Time may need tweeking
+        newTime = previous + timedelta(seconds = 20)
+        # Less than
+        if(time <= newTime):
+            newTimesList[secondNum].append(time)
+        else:
+            newTimesList.append([])
+            secondNum += 1
+            newTimesList[secondNum].append(time)
+        previous = time
+    else:
+        previous = time
+        newTimesList[secondNum].append(time)
+
+num = -1
+for element in newTimesList:
+    length = len(element) - 1
+    num += 1
+    if(length >= 6):
+        startTime = newTimesList[num][0]
+        endTime = newTimesList[num][length]
+        StartTimes.append((str(startTime) + "/" + str(endTime)))
+        
+numTime = 0
+with open("AfterTwitter.txt") as file:
+    for line in file:
+        times = StartTimes[numTime]
+        splitTime = times.split("/")
+        lineSplit = line.split()
+        if "SOCIAL MEDIA" in line:
+            lineTime = lineSplit[3] + " " + lineSplit[5]
+        else:
+            lineTime = lineSplit[2] + " " + lineSplit[4]
+        if lineTime + "000" >= splitTime[0] and lineTime + "000" <= splitTime[1]:
+            if "STREAMING" or "SOCIAL MEDIA" not in line:
+                fileOut = open("AfterBBC.txt", "a")
+                fileOut.write(str(line.rstrip() + " - " + "***ASSOCIATED BBC***"))
+                fileOut.write('\n')
+                fileOut.close()
+        else:
+            # Prints out the beginning of the file but not the end. Why?
+            fileOut = open("AfterBBC.txt", "a")
+            fileOut.write(line.rstrip())
+            fileOut.write('\n')
+            fileOut.close()
+        if lineTime + "000" == splitTime[1]:
+            print(numTime, len(StartTimes))
+            comNum = len(StartTimes) - 1
+            print(numTime, comNum)
+            if numTime != comNum:
+                numTime += 1
