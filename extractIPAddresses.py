da8b247a704c5b94fd7f2e217bf3ed1035ad937a
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-04-29 16:35:14 +0100

    Update extractIPAddresses.py
    Updated to check the name_table in the database before outputing to the ip address file

--- extractIPAddresses.py
+++ extractIPAddresses.py
@@ -1,4 +1,7 @@
-with open("0704output.txt", 'r') as captureFile:
+import mysql.connector
+from mysql.connector import Error
+connection = mysql.connector.connect(user='parker', password='password', host='192.168.20.30', database='user_profiling')
+with open("nfDumpOutput.txt", 'r') as captureFile:
     for captureLine in captureFile:
         if "Date" not in captureLine:
             if "Summary" not in captureLine:
@@ -11,12 +14,33 @@ with open("0704output.txt", 'r') as captureFile:
                                 dstIP = extractedLine[7]
                                 srcIP = srcIP.split(":", 1)[0]
                                 dstIP = dstIP.split(":", 1)[0]
-                                fileOut = open("ipAddressesPython0704.txt", 'a')
-                                fileOut.write(str(srcIP))
-                                fileOut.write('\n')
-                                fileOut.write(str(dstIP))
-                                fileOut.write('\n')
-                                fileOut.close()
-                            except:
-                                continue
-print("completed")
+                                
+                                #check the database to see if there is a match
+                                checkMatch1 = connection.cursor()
+                                query = "SELECT IP_address FROM name_table WHERE IP_address = %s"
+                                # add was added because it wouldn't work without it. Think its the comma for some reason.
+                                addr = (" " + srcIP,)
+                                checkMatch1.execute(query, addr)
+                                result = checkMatch1.fetchone()
+                                if result is None:
+                                    # output to file
+                                    fileOut = open("ipAddressesPython.txt", 'a')
+                                    fileOut.write(str(srcIP))
+                                    fileOut.write('\n')
+                                    fileOut.close()
+                                checkMatch1.close()
+                                checkMatch2 = connection.cursor()
+                                query = "SELECT IP_address FROM name_table WHERE IP_address = %s"
+                                addr2 = (" " + dstIP,)
+                                checkMatch2.execute(query, addr2)
+                                result = checkMatch2.fetchone()
+                                if result is None:
+                                    # output to file                                
+                                    fileOut = open("ipAddressesPython.txt", 'a')
+                                    fileOut.write(str(dstIP))
+                                    fileOut.write('\n')
+                                    fileOut.close()
+                                checkMatch2.close()
+                            except Error as e: 
+                                print(e)
+
