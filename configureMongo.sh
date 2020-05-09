99f46e3406c0c8f965f3c9086c7ff9f902cd8cdf
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-09 13:09:25 +0100

    Delete configureMongo.sh
    Not needed anymore

--- configureMongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt install -y mongodb
-systemctl enable mongodb
-
-mongo < createUserMongo.js
-sed -i 's/#auth = true/auth = true/g' /etc/mongodb.conf 
-ipv4=$(hostname -I)
-sed -i "s/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,$ipv4/g" /etc/mongodb.conf
