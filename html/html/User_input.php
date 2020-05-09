5459fa46bbe1605f1980d4a55370f9cd66abcdde
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-09 13:20:13 +0100

    Update User_input.php
    Added validation

--- html/html/User_input.php
+++ html/html/User_input.php
@@ -13,60 +13,88 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		 <title>User's</title>
+		<title>User's</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
 
-	<body>
-		<table style="width:100%; border:1px solid black;"">
-			<tr>
-				<th style="border:1px solid black;">User IP address</th>
-				<th style="border:1px solid black;">User</th>
-			</tr>
+	<header>
+		<ul>
+			<li><a href='index.php'>Home Page</a></li>
+      			<li><a href='flows_page.php'>Flows</a></li>
+      			<li><a href='Application_Input.php'>Applications</a></li>
+			<li><a href='User_input.php'>Users</a></li>
+		</ul>
+	</header>
+
+	<body>	
+	<h1>Users</h1>
+		<div class = "column">
+			<h2>View users</h2>
 			<?php
 				$userSelect = mysqli_query($conn, "SELECT*FROM Users;");
 				if(mysqli_num_rows($userSelect) > 0){
+					echo " <table>
+						<tr>
+						<th>IP Address</th>
+						<th>Name</th>
+						<th>Action</th>
+						</tr>";
 					while($row = mysqli_fetch_array($userSelect)){
+						$oldIP = $row['User_IP_addr'];
+						$olduname = $row['User_Name'];
+						echo "<form method='POST' action='edituser.php?oldIP=$oldIP'>
+						      <tr>
+						      <td><input type='text' value ='{$oldIP}' name='upIPAddr'></td>
+						      <td><input type='text' value='{$olduname}' name='uname'></td>"
 			?>
-			<tr>
-                        	<td style="border:1px solid black;"><?php echo $row["User_IP_addr"]; ?></td>
-                        	<td style="border:1px solid black;"><?php echo $row["User_Name"]; ?></td>
-                    	</tr>
+						      <td><input type='submit' value='Update' name='update' onclick="return confirm('Are you sure you want to update the record?');">
+						      <input type='submit' value='Delete' name='delete' onclick="return confirm('Are you sure you want to delete the record?');"></td>
+						      </tr>
+						     </form>
 			<?php
+		    	
 					}
+				echo "</table>";
 				}
 				else{
 					echo "No results found";
 				}
 			?>
-		</table>
-		</br>
-		</br>
+				
+			<h3> Create a user</h3>
+			<div class="INSERT">
+				<form method = "POST" action = "User_input.php" enctype="multipart/form-data">
+					<label for="user_ip_address">IP Address</label>
+					<input type = "text" name = "user_ip_address" placeholder = "IP Address" required>
+					<br>
+					<label for="name">Name</label>
+					<input type = "text" name = "name" placeholder = "Name" required>
+					<br><br>
+					<input type = "submit" name = "enter_user" value = "Create User">
+				</form>
+			<?php
+				if(isset($_POST["enter_user"])){
+					$user_IP = strip_tags(trim($_POST['user_ip_address']));
+					$name = strip_tags(trim($_POST['name']));
+					$user_IP = mysqli_real_escape_string($conn, $user_IP);
+					$name = mysqli_real_escape_string($conn, $name);
+					if(!preg_match("/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/", $user_IP)){
+      						echo "Ensure IP address is entered";
+    					}else{
+						$sql = "INSERT INTO Users (User_IP_Addr, User_Name) VALUES ('$user_IP', '$name');";
+						if(!mysqli_query($conn, $sql)){
+							echo "There was an error. Please try again";
+						}else{
+							echo "User created";
+							echo "<meta http-equiv='refresh' content='0'>";
+						}
+					}
+				} 
 
-
-		<form method = "POST" action = "User_input.php" enctype="multipart/form-data">
-			<input type = "text" name = "user_ip_address" placeholder = "IP Address" required>
-			<br>
-			<input type = "text" name = "name" placeholder = "Name" required>
-			<br><br>
-			<input type = "submit" name = "enter_user" value = "Create User">
-		</form>
-	<?php
-		if(isset($_POST["enter_user"])){
-			$user_IP = strip_tags(trim($_POST['user_ip_address']));
-			$name = strip_tags(trim($_POST['name']));
-			$user_IP = mysqli_real_escape_string($conn, $user_IP);
-			$name = mysqli_real_escape_string($conn, $name);
-
-			$sql = "INSERT INTO Users (User_IP_Addr, User_Name) VALUES ('$user_IP', '$name');";
-			mysqli_query($conn, $sql);
-			echo "User created";
-			echo "<meta http-equiv='refresh' content='0'>";
-		} 
-
-	?>
-	
-	
-
-	
+			?>
+			
+			</div>
+			
+		</div>
 	</body>
 <html>
