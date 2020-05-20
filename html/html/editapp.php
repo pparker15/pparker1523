e07f587a026ff3cdd119c0845cb2332170f50fe5
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-20 16:45:24 +0100

    Update editapp.php

--- html/html/editapp.php
+++ html/html/editapp.php
@@ -1,51 +1,57 @@
 <?php
+	# Database connection
 	$DB_SERVER = 'localhost:3306';
 	$DB_USERNAME = 'parker';
 	$DB_PASSWORD = 'password';
-	$DB_DATABASE = 'user_profiling';
+	$DB_DATABASE = 'application_identification';
 	
 	$conn = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <title></title>
-</head>
-<body>
-<?php
-  $oldIP = $_GET['oldIP'];
-  $newIP = $_POST['upIPAddr'];
-  $newuname = $_POST['uname'];
-  #if(!preg_match("/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/", $newIP)){
-  #    echo "Ensure IP address is entered";
-  #}else{
+	<head>
+  		<title></title>
+	</head>
+	<body>
 
-  #}
-  
-  
+		<?php
+  			$App_ID = $_GET['appID'];
+  			$App_name = $_POST['newAppName'];
+  			$Category = $_POST['newCategory'];
+ 			$time = $_POST['newTime'];
+  			$number_flows = $_POST['newNumber'];
 
-  if(isset($_POST['delete'])){
-    if(!mysqli_query($conn, "DELETE FROM Users WHERE User_IP_addr = '$oldIP';")){
-	echo "There was an error. Please try again.";
-    }else{
- 	echo "<meta http-equiv='refresh' content='1; url=User_input.php'/>";
-    }
-  }else{
-    if(!preg_match("/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/", $newIP)){
-      echo "Ensure IP address is entered";
-    }else{
-	if(!mysqli_query($conn, "UPDATE Users SET User_IP_addr ='$newIP', User_Name = '$newuname' WHERE User_IP_addr ='$oldIP';")){
-   	   echo "There was an error. Please try again.";
-        }else{
- 	   echo "<meta http-equiv='refresh' content='1; url=User_input.php'/>";
-        }
-    }
-    
-  }
-
-?>
+  			if(isset($_POST['delete'])){
+			# Delete application from the database.
+				if(!mysqli_query($conn, "DELETE FROM application_type WHERE App_ID = '$App_ID';")){
+					echo "There was an error. Ensure identify by records associated with this App ID is deleted first";
+				}else{
+					echo "App deleted";
+					echo "<meta http-equiv='refresh' content='1; url=Application_Input.php'/>";
+				}
+			# Update the application in the database.
+  			}else{
+	
+			  	if(empty($time) and empty($number_flows)){
+					$sql = "UPDATE application_type SET Application_name = '$App_name', Category = '$Category', time_between_flows = null, no_required_flows = null WHERE App_ID = '$App_ID';";
+				}
+				elseif(empty($time)){
+					$sql = "UPDATE application_type SET Application_name = '$App_name', Category = '$Category', time_between_flows = null, no_required_flows = $number_flows WHERE App_ID = '$App_ID';";
+				}elseif(empty($number_flows)){
+					$sql = "UPDATE application_type SET Application_name = '$App_name', Category = '$Category', time_between_flows = '$time', no_required_flows = null  WHERE App_ID = '$App_ID';";
+				}else{
+					$sql = "UPDATE application_type SET Application_name = '$App_name', Category = '$Category', time_between_flows = '$time', no_required_flows = $number_flows WHERE App_ID = '$App_ID';";
+				}
+				if(!mysqli_query($conn, $sql)){
+					echo "There was an error please try again";	
+				}else{
+					echo "App updated";
+					echo "<meta http-equiv='refresh' content='1; url=Application_Input.php'/>";
+				}
+				
+			}
+		?>
   
-</body>
+	</body>
 </html>
-
