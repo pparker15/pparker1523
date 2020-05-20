f4c218622ac93301a2207e151c657b48d5b8ac47
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-20 15:37:58 +0100

    Update flows_page.php

--- html/html/flows_page.php
+++ html/html/flows_page.php
@@ -1,23 +1,22 @@
 <?php
+	# Database connection.
 	$DB_SERVER = 'localhost:3306';
 	$DB_USERNAME = 'parker';
 	$DB_PASSWORD = 'password';
-	$DB_DATABASE = 'user_profiling';
+	$DB_DATABASE = 'application_identification';
 	
 	$conn = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-		
-	
-	
 ?>
 
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>User's</title>
+<head>
+		<title>Flows search</title>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
 
 	<header>
+		<!--Navigation Bar-->
 		<ul>
 			<li><a href='index.php'>Home Page</a></li>
       			<li><a href='flows_page.php'>Flows</a></li>
@@ -25,68 +24,88 @@
 			<li><a href='User_input.php'>Users</a></li>
 		</ul>
 	</header>
+<body>
+	<div class = "column">
+	<h1>Flows</h1>
+		<p>Search for flows using the categories provided. Select a field to use as part of the search criteria.</p>
 
-	<body>	
-	<h1>Users</h1>
-		<div class = "column">
-			<h2>View users</h2>
+		<p>*NOTE* Please wait for the page to load before scrolling through, which may take a while as there is a large amount of data. If there are no results, there may be to many rows.</p>
+
+		<!--Form to filter the flows and associated applications-->	
+		<form method = "POST" action='flows_page.php'>
+			<label for="filter">Filter: </label>
+			<label for="source">Source name</label>
+			<input type="radio" id="src_Name" name = "filter" value ="srcName">
+			<label for="Destination">Destination Name</label>
+			<input type="radio" id="Destination" name = "filter" value ="dstName">
+			<label for="DestinationSource">Source and Destination</label>
+			<input type="radio" id="DestinationSource" name = "filter" value ="dstSRC">		
+			<label for="Category">Category</label>
+			<input type="radio" id="Category" name = "filter" value ="cat">
+			<br>
+			<input type="text" placeholder="Search..." name = "search">
+			<input type='submit' value='Go' name='btnSearch'>
+		</form>
+
+	<?php
+		# Select flows from the database depending on data from the form.
+		$filter = $_POST['filter'];
+		$search = $_POST['search'];
+		if(isset($_POST['btnSearch'])){
+			switch ($filter){
+				case "srcName":
+					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Source_Name = '$search';";
+					break;
+				case "dstName":
+					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Destination_Name = '$search';";
+					break;
+				case "cat":
+					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Flow_Category = '$search';";
+					break;
+				case "associated":
+					echo"Associated coming soon";
+					break;
+				case "dstSRC":
+					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Source_Name = '$search' or c.Destination_Name = '$search'";
+					break;
+			}
+		}
+		else {
+			$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c;";
+		}
+
+	?>
+
+	<!--Display the data in a table-->
+	<table>
+			<tr>
+				<th>Flow Timestamp</th>
+				<th>Source Name</th>
+				<th>Destination Name</th>
+				<th>Category</th>
+				<th>Associated</th>
+			</tr>
 			<?php
-				$userSelect = mysqli_query($conn, "SELECT*FROM Users;");
-				if(mysqli_num_rows($userSelect) > 0){
-					echo " <table>
-						<tr>
-						<th>IP Address</th>
-						<th>Name</th>
-						<th>Action</th>
-						</tr>";
-					while($row = mysqli_fetch_array($userSelect)){
-						$oldIP = $row['User_IP_addr'];
-						$olduname = $row['User_Name'];
-						echo "<form method='POST' action='edituser.php?oldIP=$oldIP'>
-						      <tr>
-						      <td><input type='text' value ='{$oldIP}' name='upIPAddr'></td>
-						      <td><input type='text' value='{$olduname}' name='uname'></td>
-						      <td><input type='submit' value='Update' name='update'>
-						      <input type='submit' value='Delete' name='delete'></td>
-						      </tr>
-						     </form>";
-		    	
+				$appSelect = mysqli_query($conn, $sql);
+				if(mysqli_num_rows($appSelect) > 0){
+					while($row = mysqli_fetch_array($appSelect)){
+			?>
+			<tr>
+                        	<td><?php echo $row["Flow_Date_Time"]; ?></td>
+                        	<td><?php echo $row["Source_Name"]; ?></td>
+				<td><?php echo $row["Destination_Name"]; ?></td>
+				<td><?php echo $row["Flow_Category"]; ?></td>
+				<td><?php echo $row["Associated"]; ?></td>
+			</tr>
+			<?php
 					}
-				echo "</table>";
 				}
 				else{
 					echo "No results found";
 				}
 			?>
-				
-			<h3> Create a user</h3>
-			<div class="INSERT">
-				<form method = "POST" action = "User_input.php" enctype="multipart/form-data">
-					<label for="user_ip_address">IP Address</label>
-					<input type = "text" name = "user_ip_address" placeholder = "IP Address" required>
-					<br>
-					<label for="name">Name</label>
-					<input type = "text" name = "name" placeholder = "Name" required>
-					<br><br>
-					<input type = "submit" name = "enter_user" value = "Create User">
-				</form>
-			<?php
-				if(isset($_POST["enter_user"])){
-					$user_IP = strip_tags(trim($_POST['user_ip_address']));
-					$name = strip_tags(trim($_POST['name']));
-					$user_IP = mysqli_real_escape_string($conn, $user_IP);
-					$name = mysqli_real_escape_string($conn, $name);
-
-					$sql = "INSERT INTO Users (User_IP_Addr, User_Name) VALUES ('$user_IP', '$name');";
-					mysqli_query($conn, $sql);
-					echo "User created";
-					echo "<meta http-equiv='refresh' content='0'>";
-				} 
-
-			?>
-			
-			</div>
-			
+		</table>
 		</div>
-	</body>
-<html>
+		
+</body>
+</html>
