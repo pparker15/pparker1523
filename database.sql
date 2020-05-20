0205b950c70bf1d21b6b5736b34b18affc512a4b
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-20 17:10:00 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `user_profiling` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `user_profiling`;
+CREATE DATABASE  IF NOT EXISTS `application_identification` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `application_identification`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
 --
--- Host: 192.168.20.30    Database: user_profiling
+-- Host: 192.168.20.30    Database: application_identification
 -- ------------------------------------------------------
 -- Server version	5.7.29-0ubuntu0.18.04.1
 
@@ -49,7 +49,7 @@ CREATE TABLE `Flows` (
   `Destination_Name` varchar(45) DEFAULT NULL,
   `Flow_Category` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Flow_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2823997 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5213967 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -60,7 +60,7 @@ CREATE TABLE `Flows` (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`parker`@`192.168.20.44`*/ /*!50003 TRIGGER `user_profiling`.`Flows_AFTER_INSERT` AFTER INSERT ON `Flows` FOR EACH ROW
+/*!50003 CREATE*/ /*!50017 DEFINER=`parker`@`192.168.20.44`*/ /*!50003 TRIGGER `application_identification`.`Flows_AFTER_INSERT` AFTER INSERT ON `Flows` FOR EACH ROW
 BEGIN
 	UPDATE application_type Set App_Count = App_Count + 1 WHERE Application_name = new.Source_Name;
     UPDATE application_type Set App_Count = App_Count + 1 WHERE Application_name = new.Destination_Name;
@@ -87,43 +87,6 @@ CREATE TABLE `Identify_apps` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `Users`
---
-
-DROP TABLE IF EXISTS `Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Users` (
-  `User_IP_addr` varchar(45) NOT NULL,
-  `User_Name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`User_IP_addr`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`parker`@`192.168.20.44`*/ /*!50003 TRIGGER `user_profiling`.`Users_AFTER_INSERT` AFTER INSERT ON `Users` FOR EACH ROW
-
-BEGIN
-	DECLARE ID INT(11);
-	INSERT INTO application_type (application_name, Category) VALUES (new.User_Name, 'USERS');
-    SELECT App_ID INTO ID FROM application_type WHERE application_name = new.User_Name;
-    INSERT INTO Identify_apps (App_ID, identify_by) VALUES (ID, new.User_IP_Addr);
-    INSERT INTO Identify_apps (App_ID, identify_by) VALUES (ID, new.User_Name);
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-
---
 -- Table structure for table `application_type`
 --
 
@@ -138,7 +101,7 @@ CREATE TABLE `application_type` (
   `time_between_flows` varchar(45) DEFAULT NULL,
   `no_required_flows` int(11) DEFAULT NULL,
   PRIMARY KEY (`App_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=123456822 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -149,8 +112,7 @@ CREATE TABLE `application_type` (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`parker`@`192.168.20.44`*/ /*!50003 TRIGGER `user_profiling`.`application_type_AFTER_INSERT` AFTER INSERT ON `application_type` FOR EACH ROW
-
+/*!50003 CREATE*/ /*!50017 DEFINER=`parker`@`192.168.20.44`*/ /*!50003 TRIGGER `application_identification`.`application_type_AFTER_INSERT` AFTER INSERT ON `application_type` FOR EACH ROW
 BEGIN
     IF new.Category = "CDN" then
 		INSERT INTO stats_table (App_ID) VALUES (new.App_ID);
@@ -182,30 +144,6 @@ CREATE TABLE `name_table` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `processed_nfdump_data`
---
-
-DROP TABLE IF EXISTS `processed_nfdump_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `processed_nfdump_data` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Category` varchar(45) NOT NULL,
-  `Flow_date_time` varchar(45) NOT NULL,
-  `Protocol` varchar(10) NOT NULL,
-  `AS_Src` varchar(100) NOT NULL,
-  `AS_Dst` varchar(100) NOT NULL,
-  `NS_Src` varchar(100) NOT NULL,
-  `NS_Dst` varchar(100) NOT NULL,
-  `Associated_1` varchar(45) DEFAULT NULL,
-  `Associated_2` varchar(45) DEFAULT NULL,
-  `Associated_3` varchar(45) DEFAULT NULL,
-  `date_time_flow_less_specific` datetime DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=26928 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `stats_table`
 --
 
@@ -219,15 +157,15 @@ CREATE TABLE `stats_table` (
   `Assoc_Streaming` int(11) DEFAULT '0',
   PRIMARY KEY (`App_ID`),
   CONSTRAINT `fk_stats_App_ID` FOREIGN KEY (`App_ID`) REFERENCES `application_type` (`App_ID`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=123456819 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping events for database 'user_profiling'
+-- Dumping events for database 'application_identification'
 --
 
 --
--- Dumping routines for database 'user_profiling'
+-- Dumping routines for database 'application_identification'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -239,4 +177,4 @@ CREATE TABLE `stats_table` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-10 13:36:53
+-- Dump completed on 2020-05-20 17:08:36
