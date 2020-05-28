a029c7e6f43d3e17279ea8f4c46e943e64995246
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-28 01:53:05 +0100

    Update IdentificationByAssociation.py

--- scripts/IdentificationByAssociation.py
+++ scripts/IdentificationByAssociation.py
@@ -1,7 +1,8 @@
 import mysql.connector
 from mysql.connector import Error
 from datetime import datetime, timedelta, time
-connection = mysql.connector.connect(user='parker', password='password', host='192.168.20.30', database='application_identification')
+#connection = mysql.connector.connect(user='parker', password='password', host='192.168.20.30', database='application_identification')
+connection = mysql.connector.connect(user='parker', password='password', host='192.168.20.30', database='user_profiling')
 
 # Loop through the users to ensure association is kept separate for each user.
 users = connection.cursor()
@@ -32,13 +33,15 @@ for user in userResults:
 
             # Retrieve the times of all the flows for the user and application
             qApp = connection.cursor()
-            query1 = "SELECT * FROM Flows WHERE Source_Name = %s AND Destination_Name = %s or Destination_Name = %s AND Source_Name = %s";
-            data = (user[1], app[1], user[1], app[1])
+            #query1 = "SELECT * FROM Flows WHERE Source_Name = %s AND Destination_Name = %s or Destination_Name = %s AND Source_Name = %s"
+            query1 = "SELECT Flows.Flow_ID, Flows.Flow_Date_Time, Flows.Source_Name_ID, Flows.Destination_Name_ID, Flows.Flow_Category, n1.App_ID, n2.App_ID FROM Flows INNER JOIN name_table as n1 on n1.name_id = Flows.Source_Name_ID INNER JOIN name_table as n2 on n2.name_id = Flows.Destination_Name_ID WHERE (n1.App_ID = %s and n2.App_ID = %s) or (n2.App_ID = %s and n1.App_ID = %s) ORDER BY Flows.Flow_Date_Time ASC"
+            data = (user[0], app[0], user[0], app[0])
             qApp.execute(query1, data)
             result = qApp.fetchall()
             for r in result:
                 Time.append(datetime.strptime(r[1], '%Y-%m-%d %H:%M:%S.%f'))
             qApp.close()
+            
             # Check the flow is within the time limit
             for t in Time:
                 if previous != " ":
@@ -78,8 +81,9 @@ for user in userResults:
             #output to the database      
             # Get all the flows for that user between the start and end times that have the category of CDN or unknown
             getFlows = connection.cursor()
-            flowQuery = "SELECT * FROM Flows WHERE (Source_name = %s or Destination_name = %s) AND (Flow_Category = 'CDN' or Flow_Category = 'UNKNOWN')"
-            data = (user[1], user[1])
+            #flowQuery = "SELECT * FROM Flows WHERE (Source_name = %s or Destination_name = %s) AND (Flow_Category = 'CDN' or Flow_Category = 'UNKNOWN')"
+            flowQuery = "SELECT Flows.Flow_ID, Flows.Flow_Date_Time, Flows.Source_Name_ID, Flows.Destination_Name_ID, Flows.Flow_Category, n1.App_ID, n2.App_ID FROM Flows INNER JOIN name_table as n1 on n1.name_id = Flows.Source_Name_ID INNER JOIN name_table as n2 on n2.name_id = Flows.Destination_Name_ID WHERE (n1.App_ID = %s or n2.App_ID = %s) AND (Flows.Flow_Category = 'CDN' or Flows.Flow_Category = 'UNKNOWN') ORDER BY Flows.Flow_Date_Time ASC"
+            data = (user[0], user[0])
             getFlows.execute(flowQuery, data)
             result = getFlows.fetchall()
             for res in result:
@@ -100,46 +104,90 @@ for user in userResults:
 
                         # insert into stats table
                         # get current number from the stats table
-                        getStats = connection.cursor()
-                        statQuery = "SELECT * FROM stats_table"
-                        getStats.execute(statQuery)
-                        statResults = getStats.fetchall()
-                        ID = ' '
-                        if "User" in res[2]:
-                            getAppID2 = connection.cursor()
-                            newQuery = "SELECT App_ID FROM application_type WHERE Application_name = %s"
-                            dataAppID = (res[3],)
-                            getAppID2.execute(newQuery, dataAppID)
-                            resultAppID = getAppID2.fetchone()
-                            ID = resultAppID[0]
-                            getAppID2.close()
-                        elif "User" in res[3]:
-                            getAppID2 = connection.cursor()
-                            newQuery = "SELECT App_ID FROM application_type WHERE Application_name = %s"
-                            dataAppID = (res[2],)
-                            getAppID2.execute(newQuery, dataAppID)
-                            resultAppID = getAppID2.fetchone()
-                            ID = resultAppID[0]
-                            getAppID2.close()
-                        # update the current number in the stats table
-                        updateStats = connection.cursor()
-                        if app[2] == "SOCIAL MEDIA":
-                            statQuery = "UPDATE stats_table SET Assoc_Social_Media = Assoc_Social_Media + 1 WHERE App_ID = %s"
-                            dataStats = (ID,)
-                            updateStats.execute(statQuery, dataStats)
-                            connection.commit()
-                        elif app[2] == "NEWS":
-                            statQuery = "UPDATE stats_table SET Assoc_News = Assoc_News + 1 WHERE App_ID = %s"
-                            dataStats = (ID,)
-                            updateStats.execute(statQuery, dataStats)
-                            connection.commit()
-                        elif app[2] == "STREAMING":
-                            statQuery = "UPDATE stats_table SET Assoc_Streaming = Assoc_Streaming + 1 WHERE App_ID = %s"
-                            dataStats = (ID,)
-                            updateStats.execute(statQuery, dataStats)
-                            connection.commit()
-                    
-                        updateStats.close()
+
+                        # get the name_ID from source and destination
+                        # find out the category of the name_ID
+                        # if CDN or Unknown then add 1 to assocation stat within name_table
+
+
+
+                        # select the category using name_id from the flows table - for the source - repeat for destination
+                        #Where am i going to get the name_id from?
+                        #source
+                        cursor4 = connection.cursor()
+                        query4 = "SELECT application_type.Category FROM name_table INNER JOIN application_type on application_type.App_ID = name_table.App_ID WHERE name_table.name_ID = %s"
+                        data4 = (res[2],)
+                        cursor4.execute(query4, data4)
+                        result4 = cursor4.fetchone()
+                        sourceCat = result4[0]
+                        cursor4.close()
+
+                        # destination
+                        cursor5 = connection.cursor()
+                        query5 = "SELECT application_type.Category FROM name_table INNER JOIN application_type on application_type.App_ID = name_table.App_ID WHERE name_table.name_ID = %s"
+                        data5 = (res[3],)
+                        cursor5.execute(query5, data5)
+                        result5 = cursor5.fetchone()
+                        destCat = result5[0]
+                        cursor5.close()
+                            
+                       
+                        
+
+                        if sourceCat == "USERS":
+                            # add +1 to the destID name thing where assoc app = spotify as an example
+                            # need to get the app name
+                            if (app[1] == "Twitter"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Twitter = Assoc_Twitter + 1 WHERE name_id = %s"
+                                data6 = (res[3],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "Facebook"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Facebook = Assoc_Facebook + 1 WHERE name_id = %s"
+                                data6 = (res[3],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "BBC"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_BBC = Assoc_BBC + 1 WHERE name_id = %s"
+                                data6 = (res[3],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "Spotify"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Spotify = Assoc_Spotify + 1 WHERE name_id = %s"
+                                data6 = (res[3],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                        elif destCat == "USERS":
+                            if (app[1] == "Twitter"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Twitter = Assoc_Twitter + 1 WHERE name_id = %s"
+                                data6 = (res[2],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "Facebook"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Facebook = Assoc_Facebook + 1 WHERE name_id = %s"
+                                data6 = (res[2],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "BBC"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_BBC = Assoc_BBC + 1 WHERE name_id = %s"
+                                data6 = (res[2],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                            elif (app[1] == "Spotify"):
+                                updateTwitter = connection.cursor()
+                                query6 = "UPDATE name_table SET Assoc_Spotify = Assoc_Spotify + 1 WHERE name_id = %s"
+                                data6 = (res[2],)
+                                updateTwitter.execute(query6, data6)
+                                connection.commit()
+                  
+                        updateTwitter.close()
                     # ensure each application instance is associated.
                     elif res[1] + "000" == splitTime[1] or res[1] + "000" >= splitTime[1]:
                         comNum = len(StartTimes) - 1
