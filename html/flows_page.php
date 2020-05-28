19ac3a9d79587a3dfe01d5e647499a02e556e362
pparker15 <62694769+pparker15@users.noreply.github.com>
2020-05-28 02:05:45 +0100

    Update flows_page.php

--- html/flows_page.php
+++ html/flows_page.php
@@ -3,7 +3,7 @@
 	$DB_SERVER = 'localhost:3306';
 	$DB_USERNAME = 'parker';
 	$DB_PASSWORD = 'password';
-	$DB_DATABASE = 'application_identification';
+	$DB_DATABASE = 'user_profiling';
 	
 	$conn = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 ?>
@@ -26,58 +26,13 @@
 <body>
 	<div class = "column">
 	<h1>Flows</h1>
-		<p>Search for flows using the categories provided. Select a field to use as part of the search criteria.</p>
+		<p>View the flows and its associated applications</p>
 
-		<p>*NOTE* Please wait for the page to load before scrolling through, which may take a while as there is a large amount of data. If there are no results, there may be to many rows.</p>
-
-		<!--Form to filter the flows and associated applications-->	
-		<form method = "POST" action='flows_page.php'>
-			<label for="filter">Filter: </label>
-			<label for="source">Source name</label>
-			<input type="radio" id="src_Name" name = "filter" value ="srcName">
-			<label for="Destination">Destination Name</label>
-			<input type="radio" id="Destination" name = "filter" value ="dstName">
-			<label for="DestinationSource">Source and Destination</label>
-			<input type="radio" id="DestinationSource" name = "filter" value ="dstSRC">		
-			<label for="Category">Category</label>
-			<input type="radio" id="Category" name = "filter" value ="cat">
-			<br>
-			<input type="text" placeholder="Search..." name = "search">
-			<input type='submit' value='Go' name='btnSearch'>
-		</form>
-
-	<?php
-		# Select flows from the database depending on data from the form.
-		$filter = $_POST['filter'];
-		$search = $_POST['search'];
-		if(isset($_POST['btnSearch'])){
-			switch ($filter){
-				case "srcName":
-					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Source_Name = '$search';";
-					break;
-				case "dstName":
-					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Destination_Name = '$search';";
-					break;
-				case "cat":
-					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Flow_Category = '$search';";
-					break;
-				case "associated":
-					echo"Associated coming soon";
-					break;
-				case "dstSRC":
-					$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c where c.Source_Name = '$search' or c.Destination_Name = '$search'";
-					break;
-			}
-		}
-		else {
-			$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name, Destination_Name, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c;";
-		}
-
-	?>
+		<p>*NOTE* Please wait for the page to load before scrolling through, which may take a while as there is a large amount of data.</p>
 
 	<!--Display the data in a table-->
 	<table>
-			<tr>
+			<tr>		
 				<th>Flow Timestamp</th>
 				<th>Source Name</th>
 				<th>Destination Name</th>
@@ -85,14 +40,26 @@
 				<th>Associated</th>
 			</tr>
 			<?php
+				$sql= "SELECT Flow_ID, Flow_Date_Time, Source_Name_ID, Destination_Name_ID, Flow_Category,(SELECT GROUP_CONCAT(Application_name) from application_type a, Associated b where a.App_ID = b.Associated_App_ID and b.Flow_ID = c.Flow_ID) as Associated from Flows c;";
 				$appSelect = mysqli_query($conn, $sql);
 				if(mysqli_num_rows($appSelect) > 0){
 					while($row = mysqli_fetch_array($appSelect)){
+						# Get the source name	
+						$FlowID = $row["Flow_ID"];
+						$SourceQuery = "SELECT application_type.application_name FROM Flows INNER JOIN name_table on name_table.name_id = Flows.Source_Name_ID INNER JOIN application_type on application_type.App_ID = name_table.App_ID WHERE Flows.Flow_ID = '$FlowID'";
+						$SourceNameSelect = mysqli_query($conn, $SourceQuery);
+						$newRow = mysqli_fetch_array($SourceNameSelect);
+
+						# Get the destination name
+						$DestinationQuery = "SELECT application_type.application_name FROM Flows INNER JOIN name_table on name_table.name_id = Flows.Destination_Name_ID INNER JOIN application_type on application_type.App_ID = name_table.App_ID WHERE Flows.Flow_ID = '$FlowID'";
+						$DestinationNameSelect = mysqli_query($conn, $DestinationQuery);
+						$newRow2 = mysqli_fetch_array($DestinationNameSelect);
+
 			?>
-			<tr>
-                        	<td><?php echo $row["Flow_Date_Time"]; ?></td>
-                        	<td><?php echo $row["Source_Name"]; ?></td>
-				<td><?php echo $row["Destination_Name"]; ?></td>
+			<tr>                    	
+				<td><?php echo $row["Flow_Date_Time"]; ?></td>
+				<td><?php echo $newRow["application_name"]; ?></td>
+				<td><?php echo $newRow2["application_name"]; ?></td>
 				<td><?php echo $row["Flow_Category"]; ?></td>
 				<td><?php echo $row["Associated"]; ?></td>
 			</tr>
@@ -108,3 +75,5 @@
 		
 </body>
 </html>
+
+
